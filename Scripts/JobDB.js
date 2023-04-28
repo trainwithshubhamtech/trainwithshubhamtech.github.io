14c1af38fa69115655cb482239f174e23173eab4
trainwithshubhamtech <126598052+trainwithshubhamtech@users.noreply.github.com>
2023-04-28 14:16:14 +0530

    Create JobDB.js

---
+++ Scripts/JobDB.js
@@ -0,0 +1,44 @@
+const jobList = [
+    {
+      jobTitle: "Software Engineer",
+      companyName: "Google",
+      location: "Mountain View, CA",
+      yearsOfExperienceRequired: 5,
+      applyURL: "https://careers.google.com/jobs"
+    },
+    {
+      jobTitle: "DevOps Engineer",
+      companyName: "Amazon",
+      location: "Seattle, WA",
+      yearsOfExperienceRequired: 3,
+      applyURL: "https://www.amazon.jobs/en-gb"
+    },
+    {
+      jobTitle: "Data Analyst",
+      companyName: "Facebook",
+      location: "Menlo Park, CA",
+      yearsOfExperienceRequired: 2,
+      applyURL: "https://www.facebook.com/careers/"
+    }
+];
+
+jobList.forEach(job => {
+    AddJobData(job.jobTitle, job.companyName, job.location, job.yearsOfExperienceRequired, job.applyURL)
+  });
+  
+
+function AddJobData(jt, jc, jl, jy, ju) {
+    var divElement = `
+    <div class="panel panel-default">
+          <div class="panel-body">
+            <h3>`+jt+`</h3>
+            <h4>`+jc+`</h4>
+            <p>Location : `+jl+`</p>
+            <p>Years of Experience Required : `+jy+`</p>
+            <button type="button" class="btn btn-primary" onclick="location.href='`+ju+`'">Apply Now</button>
+          </div>
+    </div>
+    `
+    $('#lelo').append(divElement)
+}
+  
